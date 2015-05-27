1a09faed7a2685139417e8a7af0bc40018ab074c
pkoch <pkoch@bowdoin.edu>
2015-05-27 11:53:32 -0400

    updated to new tool

--- src/nbtool/src/behavior_sim/BehaviorInterpreter.java
+++ src/nbtool/src/behavior_sim/BehaviorInterpreter.java
@@ -5,17 +5,21 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 import nbtool.data.Log;
-import nbtool.io.CppIO;
-import nbtool.io.CppIO.CppFuncCall;
-import nbtool.io.CppIO.CppFuncListener;
+import nbtool.io.CommonIO.IOInstance;
+import nbtool.io.CrossIO;
+import nbtool.io.CrossIO.CrossCall;
+import nbtool.io.CrossIO.CrossFunc;
+import nbtool.io.CrossIO.CrossInstance;
+import nbtool.util.Center;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+
 import messages.*;
 
     // TODO eventually all of the protobufs need to be set
     // in a realistic way, not just giving perfect data
 
-public class BehaviorInterpreter implements CppFuncListener
+public class BehaviorInterpreter implements nbtool.io.CommonIO.IOFirstResponder
 {
     private boolean runSim;    // should the sim keep running?
 
@@ -37,146 +41,23 @@ public class BehaviorInterpreter implements CppFuncListener
 
     public BehaviorInterpreter() 
     { 
-        CppIO.ref();        // open an nbcross connection
+    	Center.startCenter();
+    	CrossIO.startCrossServer();
 
         runSim = false;     // shouldn't be running yet
 
         pQ = new LinkedList();
     }
-
-    public void run()
-    {
-        runSim = true;  // now it can run
-        this.sendMessagesInSeparateThread();        
-    }
-
-    public void initAndRun(World world)
-    {
-        runSim = true;  // now it can run
-
-        // need to send information about these to the BehaviorsModule
-        this.world = world;
-        ball = world.ball;
-        players = world.players;
-
-        byte[] bArray = {(byte)100};
-        Log logg = new Log("type=int", bArray);
-
-        CppFuncCall clearSim = new CppFuncCall();
-        clearSim.index = CppIO.current.indexOfFunc("InitSim");
-        clearSim.name = "InitSim";
-        clearSim.args = new ArrayList<Log>(Arrays.asList(logg));
-        clearSim.listener = this;        
-
-        CppIO.current.tryAddCall(clearSim);
-
-        int numPlayers = 0;
-
-        for (Player p : players) 
-        { 
-            if (p != null) 
-            {
-                numPlayers++;
-
-                byte[] byteArray = {(byte)p.num};
-                Log log = new Log("type=int", byteArray);
-
-                CppFuncCall initSim = new CppFuncCall();
-                initSim.index = CppIO.current.indexOfFunc("InitSim");
-                initSim.name = "InitSim";
-                initSim.args = new ArrayList<Log>(Arrays.asList(log));
-                initSim.listener = this;        
-
-                CppIO.current.tryAddCall(initSim);
-            }
-        }
-
-        runSpeed = FieldConstants.RUN_SPEED * numPlayers;
-
-        this.sendMessagesInSeparateThread();
-    }
-
-    // stops the function requests to C++ and results in killing the thread
-    public void end() { runSim = false; }
-
-    // prepares and send the world state info as Protobufs to C++
-    private void sendMessages()
-    {
-        tTot = System.nanoTime();
-        tBM = System.nanoTime();
-        // each player calls its own BehaviorsModule
-        for (int i = 0; i < players.length; i++)
-        {
-            if (players[i] != null)
-            {   
-                pQ.add(i);
-
-                Player player = new Player(players[i]); // make a copy so we can edit it
-                Ball b = new Ball(ball);
-
-                if (player.team) b.moveTo(b.x, b.flipY());
-                else b.moveTo(b.flipX(), b.y);
-
-                ArrayList<Log> protobufs = new ArrayList<Log>();
-                protobufs.add(new Log("type=RobotLocation", this.setLocalization(player).toByteArray()));
-                protobufs.add(new Log("type=FilteredBall", this.setFilteredBall(player, b).toByteArray()));
-                protobufs.add(new Log("type=GameState", this.setGameState(player).toByteArray()));
-                protobufs.add(new Log("type=VisionField", this.setVisionField().toByteArray()));
-                protobufs.add(new Log("type=VisionRobot", this.setVisionRobot().toByteArray()));
-                protobufs.add(new Log("type=VisionObstacle", this.setVisionObstacle().toByteArray()));
-                protobufs.add(new Log("type=FallStatus", this.setFallStatus().toByteArray()));
-                protobufs.add(new Log("type=MotionStatus", this.setMotionStatus().toByteArray()));
-                protobufs.add(new Log("type=RobotLocation", this.setOdometry().toByteArray()));
-                protobufs.add(new Log("type=Joints", this.setJoints().toByteArray()));
-                protobufs.add(new Log("type=StiffStatus", this.setStiffStatus().toByteArray()));
-                protobufs.add(new Log("type=Obstacle", this.setObstacle().toByteArray()));
-                protobufs.add(new Log("type=SharedBall", this.setSharedBall(b).toByteArray()));
-                protobufs.add(new Log("type=RobotLocation", this.setSharedFlip().toByteArray()));
-                
-                // create the function call
-                CppFuncCall funcCall = new CppFuncCall();
-                funcCall.index = CppIO.current.indexOfFunc("Behaviors");
-                funcCall.name = "Behaviors";
-                funcCall.args = protobufs;
-                funcCall.listener = this;        
-
-                // System.out.println("Build Time: " + (System.nanoTime() - tBM)/1000000.);
-                tFC = System.nanoTime();
-                CppIO.current.tryAddCall(funcCall);     // execute the function call
-            }
-
-            if (!runSim) break; // stop making function calls
-        }
-    }
-
-    // make a function request to C++ in a separate thread. Allows other proccesses (like the GUI)
-    // to keep running while we wait to make function requests
-    private void sendMessagesInSeparateThread()
-    {
-        callThread = new Thread() {
-            public void run() { 
-                while (runSim) 
-                {
-                    sendMessages();
-                    // runSim = false;
-                    // return;
-                    // time delay before next call
-                    try { callThread.sleep(runSpeed); }
-                    catch (InterruptedException e) { 
-                        System.out.println("Thread interrupted."); 
-                    }
-                }
-                sendMessages();
-                return; 
-            }
-        };
-        callThread.start();
-    }
-
+    
     @Override
-    public void returned(int ret, Log... out) 
-    {
-        // System.out.println("Func Call Time: " + (System.nanoTime() - tFC)/1000000.);
+	public void ioFinished(IOInstance instance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ioReceived(IOInstance inst, int ret, Log... out) {
+		 // System.out.println("Func Call Time: " + (System.nanoTime() - tFC)/1000000.);
         tGUI = System.nanoTime();
 
         if (out.length == 0) return;
@@ -287,6 +168,131 @@ public class BehaviorInterpreter implements CppFuncListener
 
         // System.out.println("GUI time: " + (System.nanoTime() - tGUI)/1000000.);
         // System.out.println("Total time: " + (System.nanoTime() - tTot)/1000000.);
+		
+	}
+
+	@Override
+	public boolean ioMayRespondOnCenterThread(IOInstance inst) {
+		return false;
+	}
+
+    public void run()
+    {
+        runSim = true;  // now it can run
+        this.sendMessagesInSeparateThread();        
+    }
+
+    public void initAndRun(World world)
+    {
+        runSim = true;  // now it can run
+
+        // need to send information about these to the BehaviorsModule
+        this.world = world;
+        ball = world.ball;
+        players = world.players;
+
+        byte[] bArray = {(byte)100};
+        Log logg = new Log("type=int", bArray);
+        
+        //Phil's dumb names.
+        CrossInstance ci = CrossIO.instanceByIndex(0);
+        CrossFunc cf = ci.functionWithName("InitSim");
+        CrossCall clearSim = new CrossCall(this, cf, logg);
+        ci.tryAddCall(clearSim);
+        
+        int numPlayers = 0;
+
+        for (Player p : players) 
+        { 
+            if (p != null) 
+            {
+                numPlayers++;
+
+                byte[] byteArray = {(byte)p.num};
+                Log log = new Log("type=int", byteArray);
+                
+                CrossCall is = new CrossCall(this, cf, log);
+                ci.tryAddCall(is);
+            }
+        }
+
+        runSpeed = FieldConstants.RUN_SPEED * numPlayers;
+
+        this.sendMessagesInSeparateThread();
+    }
+
+    // stops the function requests to C++ and results in killing the thread
+    public void end() { runSim = false; }
+
+    // prepares and send the world state info as Protobufs to C++
+    private void sendMessages()
+    {
+        tTot = System.nanoTime();
+        tBM = System.nanoTime();
+        // each player calls its own BehaviorsModule
+        for (int i = 0; i < players.length; i++)
+        {
+            if (players[i] != null)
+            {   
+                pQ.add(i);
+
+                Player player = new Player(players[i]); // make a copy so we can edit it
+                Ball b = new Ball(ball);
+
+                if (player.team) b.moveTo(b.x, b.flipY());
+                else b.moveTo(b.flipX(), b.y);
+
+                ArrayList<Log> protobufs = new ArrayList<Log>();
+                protobufs.add(new Log("type=RobotLocation", this.setLocalization(player).toByteArray()));
+                protobufs.add(new Log("type=FilteredBall", this.setFilteredBall(player, b).toByteArray()));
+                protobufs.add(new Log("type=GameState", this.setGameState(player).toByteArray()));
+                protobufs.add(new Log("type=VisionField", this.setVisionField().toByteArray()));
+                protobufs.add(new Log("type=VisionRobot", this.setVisionRobot().toByteArray()));
+                protobufs.add(new Log("type=VisionObstacle", this.setVisionObstacle().toByteArray()));
+                protobufs.add(new Log("type=FallStatus", this.setFallStatus().toByteArray()));
+                protobufs.add(new Log("type=MotionStatus", this.setMotionStatus().toByteArray()));
+                protobufs.add(new Log("type=RobotLocation", this.setOdometry().toByteArray()));
+                protobufs.add(new Log("type=Joints", this.setJoints().toByteArray()));
+                protobufs.add(new Log("type=StiffStatus", this.setStiffStatus().toByteArray()));
+                protobufs.add(new Log("type=Obstacle", this.setObstacle().toByteArray()));
+                protobufs.add(new Log("type=SharedBall", this.setSharedBall(b).toByteArray()));
+                protobufs.add(new Log("type=RobotLocation", this.setSharedFlip().toByteArray()));
+                
+                CrossInstance ci = CrossIO.instanceByIndex(0);
+                CrossFunc cf = ci.functionWithName("Behaviors");
+                CrossCall funcCall = new CrossCall(this, cf, protobufs.toArray(new Log[0]));      
+
+                // System.out.println("Build Time: " + (System.nanoTime() - tBM)/1000000.);
+                tFC = System.nanoTime();
+                ci.tryAddCall(funcCall);     // execute the function call
+            }
+
+            if (!runSim) break; // stop making function calls
+        }
+    }
+
+    // make a function request to C++ in a separate thread. Allows other proccesses (like the GUI)
+    // to keep running while we wait to make function requests
+    private void sendMessagesInSeparateThread()
+    {
+        callThread = new Thread() {
+            public void run() { 
+                while (runSim) 
+                {
+                    sendMessages();
+                    // runSim = false;
+                    // return;
+                    // time delay before next call
+                    try { callThread.sleep(runSpeed); }
+                    catch (InterruptedException e) { 
+                        System.out.println("Thread interrupted."); 
+                    }
+                }
+                sendMessages();
+                return; 
+            }
+        };
+        callThread.start();
     }
 
     private RobotLocationOuterClass.RobotLocation setLocalization(Player p)
